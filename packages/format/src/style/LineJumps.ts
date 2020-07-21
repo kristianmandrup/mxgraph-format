db9a14de6a5dafdd1a06227ecfe668d0c1077af2
Kristian Mandrup <kmandrup@gmail.com>
2020-07-21 21:27:56 +0100

    refactor style panel fully

--- packages/format/src/style/LineJumps.ts
+++ packages/format/src/style/LineJumps.ts
@@ -6,120 +6,192 @@ export class LineJumps extends BaseStyleFormat {
   lineJumpsEnabled: any; // Graph.lineJumpsEnabled
   defaultJumpSize: any; // Graph.defaultJumpSize
 
+  jumpSize: any;
+  styleSelect: any;
+
+  listener: any;
+
   /**
    * Adds UI for configuring line jumps.
    */
   addLineJumps(container) {
-    var ss = this.format.getSelectionState();
+    const { enableLineJumps, disableLineJumps } = this;
+    enableLineJumps() || disableLineJumps();
 
-    if (
+    return container;
+  }
+
+  disableLineJumps() {
+    if (this.shouldEnableLinejumps) return;
+    this.container.style.display = "none";
+    return true;
+  }
+
+  get shouldEnableLinejumps() {
+    const { ss } = this;
+    return (
       this.lineJumpsEnabled &&
       ss.edges.length > 0 &&
       ss.vertices.length == 0 &&
       ss.lineJumps
-    ) {
-      container.style.padding = "8px 0px 24px 18px";
+    );
+  }
 
-      var ui = this.editorUi;
-      var editor = ui.editor;
-      var graph = editor.graph;
+  appendTitle() {
+    const { container } = this;
+    const span = document.createElement("div");
+    span.style.position = "absolute";
+    span.style.fontWeight = "bold";
+    span.style.width = "80px";
 
-      var span = document.createElement("div");
-      span.style.position = "absolute";
-      span.style.fontWeight = "bold";
-      span.style.width = "80px";
+    mxUtils.write(span, mxResources.get("lineJumps"));
+    container.appendChild(span);
+  }
 
-      mxUtils.write(span, mxResources.get("lineJumps"));
-      container.appendChild(span);
+  createStyleSelect() {
+    const styleSelect = document.createElement("select");
+    styleSelect.style.position = "absolute";
+    styleSelect.style.marginTop = "-2px";
+    styleSelect.style.right = "76px";
+    styleSelect.style.width = "62px";
+    this.styleSelect = styleSelect;
+    return styleSelect;
+  }
 
-      var styleSelect = document.createElement("select");
-      styleSelect.style.position = "absolute";
-      styleSelect.style.marginTop = "-2px";
-      styleSelect.style.right = "76px";
-      styleSelect.style.width = "62px";
+  addStyleSelectOptions() {
+    const { styleSelect } = this;
+    var styles = ["none", "arc", "gap", "sharp"];
+    for (var i = 0; i < styles.length; i++) {
+      var styleOption = document.createElement("option");
+      styleOption.setAttribute("value", styles[i]);
+      mxUtils.write(styleOption, mxResources.get(styles[i]));
+      styleSelect.appendChild(styleOption);
+    }
+  }
 
-      var styles = ["none", "arc", "gap", "sharp"];
+  styleContainer() {
+    this.container.style.padding = "8px 0px 24px 18px";
+  }
 
-      for (var i = 0; i < styles.length; i++) {
-        var styleOption = document.createElement("option");
-        styleOption.setAttribute("value", styles[i]);
-        mxUtils.write(styleOption, mxResources.get(styles[i]));
-        styleSelect.appendChild(styleOption);
+  addStyleSelectChangeHandler() {
+    const { styleSelect, graph, ui } = this;
+    mxEvent.addListener(styleSelect, "change", function (evt) {
+      graph.getModel().beginUpdate();
+      try {
+        graph.setCellStyles(
+          "jumpStyle",
+          styleSelect.value,
+          graph.getSelectionCells()
+        );
+        ui.fireEvent(
+          new mxEventObject(
+            "styleChanged",
+            "keys",
+            ["jumpStyle"],
+            "values",
+            [styleSelect.value],
+            "cells",
+            graph.getSelectionCells()
+          )
+        );
+      } finally {
+        graph.getModel().endUpdate();
       }
 
-      mxEvent.addListener(styleSelect, "change", function (evt) {
-        graph.getModel().beginUpdate();
-        try {
-          graph.setCellStyles(
-            "jumpStyle",
-            styleSelect.value,
-            graph.getSelectionCells()
-          );
-          ui.fireEvent(
-            new mxEventObject(
-              "styleChanged",
-              "keys",
-              ["jumpStyle"],
-              "values",
-              [styleSelect.value],
-              "cells",
-              graph.getSelectionCells()
-            )
-          );
-        } finally {
-          graph.getModel().endUpdate();
-        }
+      mxEvent.consume(evt);
+    });
+  }
 
-        mxEvent.consume(evt);
-      });
+  addStyleSelectClickHandler() {
+    const { styleSelect } = this;
+    // Stops events from bubbling to color option event handler
+    mxEvent.addListener(styleSelect, "click", (evt) => {
+      mxEvent.consume(evt);
+    });
+  }
 
-      // Stops events from bubbling to color option event handler
-      mxEvent.addListener(styleSelect, "click", (evt) => {
-        mxEvent.consume(evt);
-      });
+  addStyleSelectHandlers() {
+    const { addStyleSelectChangeHandler, addStyleSelectClickHandler } = this;
+    addStyleSelectChangeHandler();
+    addStyleSelectClickHandler();
+  }
 
-      container.appendChild(styleSelect);
+  appendStyleSelect() {
+    const {
+      createStyleSelect,
+      addStyleSelectOptions,
+      addStyleSelectHandlers,
+    } = this;
+    createStyleSelect();
+    addStyleSelectOptions();
+    addStyleSelectHandlers();
 
-      var jumpSizeUpdate;
+    this.container.appendChild(this.styleSelect);
+  }
 
-      var jumpSize = this.addUnitInput(container, "pt", 22, 33, () => {
-        jumpSizeUpdate.apply(this, arguments);
-      });
+  createJumpSize() {
+    const { container } = this;
+    let jumpSizeUpdate;
+    const jumpSize = this.addUnitInput(container, "pt", 22, 33, () => {
+      jumpSizeUpdate.apply(this, arguments);
+    });
+    this.jumpSize = jumpSize;
 
-      jumpSizeUpdate = this.installInputHandler(
-        jumpSize,
-        "jumpSize",
-        this.defaultJumpSize,
-        0,
-        999,
-        " pt"
-      );
+    jumpSizeUpdate = this.installInputHandler(
+      jumpSize,
+      "jumpSize",
+      this.defaultJumpSize,
+      0,
+      999,
+      " pt"
+    );
+  }
 
-      var listener = (_sender?, _evt?, force?) => {
-        ss = this.format.getSelectionState();
-        styleSelect.value = mxUtils.getValue(ss.style, "jumpStyle", "none");
+  addListener() {
+    const { ss, styleSelect, jumpSize, graph } = this;
+    const listener = (_sender?, _evt?, force?) => {
+      styleSelect.value = mxUtils.getValue(ss.style, "jumpStyle", "none");
 
-        if (force || document.activeElement != jumpSize) {
-          var tmp = parseInt(
-            mxUtils.getValue(ss.style, "jumpSize", this.defaultJumpSize)
-          );
-          jumpSize.value = isNaN(tmp) ? "" : tmp + " pt";
-        }
-      };
+      if (force || document.activeElement != jumpSize) {
+        var tmp = parseInt(
+          mxUtils.getValue(ss.style, "jumpSize", this.defaultJumpSize)
+        );
+        jumpSize.value = isNaN(tmp) ? "" : tmp + " pt";
+      }
+    };
 
-      this.addKeyHandler(jumpSize, listener);
+    this.addKeyHandler(jumpSize, listener);
 
-      graph.getModel().addListener(mxEvent.CHANGE, listener);
-      this.listeners.push({
-        destroy: function () {
-          graph.getModel().removeListener(listener);
-        },
-      });
-      listener();
-    } else {
-      container.style.display = "none";
-    }
+    graph.getModel().addListener(mxEvent.CHANGE, listener);
+  }
 
-    return container;
+  registerListner() {
+    const { listeners, listener, graph } = this;
+    listeners.push({
+      destroy: function () {
+        graph.getModel().removeListener(listener);
+      },
+    });
+  }
+
+  enableLineJumps() {
+    const { shouldEnableLinejumps } = this;
+    if (shouldEnableLinejumps) return;
+    const {
+      styleContainer,
+      appendTitle,
+      appendStyleSelect,
+      createJumpSize,
+      addListener,
+    } = this;
+    styleContainer();
+    appendTitle();
+    appendStyleSelect();
+    createJumpSize();
+    addListener();
+
+    this.listener();
+
+    return true;
   }
 }
--- packages/format/src/style/StyleOps.ts
+++ packages/format/src/style/StyleOps.ts
@@ -3,14 +3,11 @@ import mx from "@mxgraph-app/mx";
 const { mxResources, mxUtils } = mx;
 
 export class StyleOps extends BaseStyleFormat {
-  /**
-   * Adds the label menu items to the given menu and parent.
-   */
-  addStyleOps(div) {
-    div.style.paddingTop = "10px";
-    div.style.paddingBottom = "10px";
+  div: any;
 
-    var btn = mxUtils.button(mxResources.get("setAsDefaultStyle"), (_evt) => {
+  appendSetAsDefaultStyleBtn() {
+    const { div } = this;
+    const btn = mxUtils.button(mxResources.get("setAsDefaultStyle"), (_evt) => {
       this.editorUi.actions.get("setAsDefaultStyle").funct();
     });
 
@@ -23,7 +20,23 @@ export class StyleOps extends BaseStyleFormat {
     );
     btn.style.width = "202px";
     div.appendChild(btn);
+  }
+
+  /**
+   * Adds the label menu items to the given menu and parent.
+   */
+  addStyleOps(div) {
+    this.div = div;
+    const { styleDiv, appendSetAsDefaultStyleBtn } = this;
+    styleDiv();
+    appendSetAsDefaultStyleBtn();
 
     return div;
+  }
+
+  styleDiv() {
+    const { div } = this;
+    div.style.paddingTop = "10px";
+    div.style.paddingBottom = "10px";
   }
 }
